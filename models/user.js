1e05f12888fda983e125be6141c63c6e97333563
Lilya Schedrina <13cutepie@gmail.com>
2023-05-17 19:18:53 +0400

    add validator pkg
    подключить пакет validator, добавить валидацию url картинок

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
   },
 });
 
