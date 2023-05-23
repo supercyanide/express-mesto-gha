392f12f73e8060eb06d5e1478c966199c52fe1ba
Lilya Schedrina <13cutepie@gmail.com>
2023-05-23 23:16:36 +0400

    review bug fix

--- app.js
+++ app.js
@@ -1,53 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+
 const userRouter = require('./routes/users');
+const signInRouter = require('./routes/signin');
+const signUpRouter = require('./routes/signup');
 const cardRouter = require('./routes/cards');
-const { createUser, login } = require('./controllers/users');
-const { NOT_FOUND_ERROR_STATUS } = require('./utils/statusConstants');
+
 const auth = require('./middlewares/auth');
+
+const NotFoundError = require('./errors/NotFoundError');
+const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 const app = express();
 app.use(express.json());
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
-  }),
-}), createUser);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-
 app.use(helmet());
 app.use(cookieParser());
+
+app.use(signInRouter);
+app.use(signUpRouter);
+
 app.use(auth);
+
 app.use(userRouter);
 app.use(cardRouter);
-app.use('*', (req, res) => {
-  res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'URL не найден' });
+
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('URL не найден'));
 });
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-  next();
-});
-
+app.use(errorHandler);
 app.listen(PORT);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const r = require('../utils/regex');
 const {
   getUsers,
   getUser,
@@ -27,7 +28,7 @@ router.patch('/users/me', celebrate({
 
 router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
+    avatar: Joi.string().regex(r),
   }),
 }), updateProfileAvatar);
 
